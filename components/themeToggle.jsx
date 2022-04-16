a39888d8a83f7e4c563379a28953fcab435a7d91
kate <christiannparanas@gmail.com>
2022-04-16 17:26:50 +0800

    moon sun graphic

--- components/themeToggle.jsx
+++ components/themeToggle.jsx
@@ -1,19 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import Image from "next/image";
+
+const moon = "/graphics/moon.svg";
+const sun = "/graphics/sun.svg";
 
 function ThemeToggle() {
-  const [isDarkMode, setIsDarkMode] = useState(false)
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
   const changeTheme = () => {
-    if(isDarkMode) return setIsDarkMode(false)
+    if (isDarkMode) return setIsDarkMode(false);
 
-    setIsDarkMode(true)
-  }
+    setIsDarkMode(true);
+  };
+
+  const parseImg = (imgSrc) => {
+    return <Image src={imgSrc} width={23} height={23} />;
+  };
 
   return (
-    <div onClick={changeTheme} className="flex justify-center items-center">
-      {isDarkMode ? "🌙" : "☀️" }
+    <div
+      onClick={changeTheme}
+      className="flex mx-auto items-center bg-slate-800 w-fit p-2 rounded-lg cursor-pointer"
+    >
+      {isDarkMode ? parseImg(sun) : parseImg(moon)}
     </div>
-  )
+  );
 }
 
-export default ThemeToggle
+export default ThemeToggle;
