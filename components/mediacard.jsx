ca8ff014c5127f98cc663439fa922faf0fd654a9
kate <christiannparanas@gmail.com>
2022-04-17 01:37:31 +0800

    fadeIn

--- components/mediacard.jsx
+++ components/mediacard.jsx
@@ -1,15 +1,18 @@
 import Image from "next/image";
+import FadeIn from "react-fade-in";
 
 const TMDB_IMG_BASE_URL = "https://image.tmdb.org/t/p/w342";
 
 function MediaCard({ media }) {
   return (
-    <div className="">
-      <img
-        className="w-full rounded-md shadow-lg"
-        src={`${TMDB_IMG_BASE_URL}${media.poster_path}`}
-      />
-    </div>
+    <FadeIn>
+      <div className="">
+        <img
+          className="w-full rounded-md shadow-lg"
+          src={`${TMDB_IMG_BASE_URL}${media.poster_path}`}
+        />
+      </div>
+    </FadeIn>
   );
 }
 
