343f24a2ec6623dbac43c5889a20942d78b0a540
kate <christiannparanas@gmail.com>
2022-05-29 12:29:56 +0800

    feat: media card group hover

--- components/MediaCard.jsx
+++ components/MediaCard.jsx
@@ -24,10 +24,10 @@ function MediaCard({ mediaType, media }) {
         passHref
       >
         <div
-          className="relative cursor-pointer hover:drop-shadow-lg min-w-[150px] w-full h-[350px]"
+          className="relative cursor-pointer hover:drop-shadow-lg min-w-[150px] w-full h-[350px] group"
         >
           <Image
-            className="rounded-md shadow-lg z-10 w-full h-[280px] sm:h-[350px]"
+            className="rounded-md shadow-lg z-10 w-full h-[280px] sm:h-[350px] group-hover:scale-110 transition-all"
             src={media.poster_path == null ? "/placeholder.svg" : src}
             placeholder="blur"
             blurDataURL="/"
